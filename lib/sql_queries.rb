42e1fa6a71ccc0e34dd97dc618d20b3e637216e6
md mia <sharifmia091@gmail.com>
2018-09-01 15:35:14 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,21 +11,21 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT name, age, SUM(amount) FROM users JOIN pledges ON users.id=pledges.user_id GROUP BY users.name ORDER BY users.name ASC;"
+  #{}"SELECT name, age, SUM(amount) FROM users JOIN pledges ON users.id=pledges.user_id GROUP BY users.name ORDER BY users.name ASC;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT title, (SUM(pledges.amount) - funding_goal) AS amount_over_goal FROM projects JOIN pledges ON projects.id=pledges.project_id GROUP BY projects.title HAVING amount_over_goal>=0"
+  #{}"SELECT title, (SUM(pledges.amount) - funding_goal) AS amount_over_goal FROM projects JOIN pledges ON projects.id=pledges.project_id GROUP BY projects.title HAVING amount_over_goal>=0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-  "SELECT name, SUM(pledges.amount) AS user_amount FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY user_amount ASC, users.name ASC;"
+  #{}"SELECT name, SUM(pledges.amount) AS user_amount FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY user_amount ASC, users.name ASC;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "SELECT category, pledges.amount FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE category = 'music';"
+  #{}"SELECT category, pledges.amount FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "SELECT category, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE category = 'books';"
+  #{}"SELECT category, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE category = 'books';"
 end
